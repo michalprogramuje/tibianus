63d7fcc1f29ae37ec87bd8442f199329fb073a2c
sparrow3kk <sparrowekk@protonmail.com>
2022-05-26 23:55:00 +0200

    db settings

--- tibianus/tibianus/db_settings/example.db_settings.py
+++ tibianus/tibianus/db_settings/example.db_settings.py
@@ -1,3 +1,5 @@
+# zmien nazwe tego pliku na db_settings (jego import znajduje sie w pliku settings.py)
+
 NAME = 'tibianus' # to zostawiasz
 USER = 'user_name' # nazwa uzytkownika bazy danych -> domyslnie jest to postgres
 PASSWORD = 'user_password' # haslo do serwera z baza danych -> ustawiasz przy konfiguracji postgressa
