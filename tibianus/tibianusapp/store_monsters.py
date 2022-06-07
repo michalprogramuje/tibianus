29f8303b6c9b5400cef09a10682640a0b9f66c05
sparrow3kk <sparrowekk@protonmail.com>
2022-06-08 01:20:35 +0200

    obsluga bledow w store_monsters

--- tibianus/tibianusapp/store_monsters.py
+++ tibianus/tibianusapp/store_monsters.py
@@ -33,61 +33,74 @@ def get_monster_detail_data(url, monster) -> Union[requests.Response, int]:
 
 
 def connector(db_settings) -> Union[int, psycopg2.connect]:
-    connection = psycopg2.connect(
-        user=db_settings.USER,
-        password=db_settings.PASSWORD,
-        host=db_settings.HOST,
-        port = db_settings.PORT,
-        database = db_settings.NAME
-    )
-    if connection.status != 1:
-        return connection.status
-    return connection
+    
+    try:
+        connection = psycopg2.connect(
+            user=db_settings.USER,
+            password=db_settings.PASSWORD,
+            host=db_settings.HOST,
+            port = db_settings.PORT,
+            database = db_settings.NAME
+        )
+    except (Exception, psycopg2.Error) as error:
+        return error
+    else:
+        return connection
 
 
 def make_query(url):
 
-    count = 0
+    count_rows = 0
+    omitted = 0
     try:
         connection = connector(db_settings)
-        cursor = connection.cursor()
-        
-        monsters = get_monsters_data(url)
-        
-        for monster in monsters:
 
-            searched_monster = """SELECT monster_name FROM tibianusapp_monster WHERE monster_name = %s"""
-            cursor.execute(searched_monster, (monster,))
-            record = cursor.fetchone()
+        if type(connection) == psycopg2.OperationalError:
+            print(connection)
+            exit()
+        else:
+            cursor = connection.cursor()
+            
+            monsters = get_monsters_data(url)
+            
+            for monster in monsters:
 
-            if record == None:
-                current_monster = get_monster_detail_data(url, monster)
-                query = """INSERT INTO tibianusapp_monster (monster_name, monster_exp) VALUES (%s, %s)"""
-                
-                if('exp' in current_monster):                
-                    if '?' in current_monster['exp'] or '--' in current_monster['exp']:
-                        continue
-                    else:
-                        if '~' in current_monster['exp']:
-                            records = (current_monster['name'], current_monster['exp'].replace('~', ''))
+                searched_monster = """SELECT monster_name FROM tibianusapp_monster WHERE monster_name = %s"""
+                cursor.execute(searched_monster, (monster,))
+                record = cursor.fetchone()
+
+                if record == None:
+                    current_monster = get_monster_detail_data(url, monster)
+                    query = """INSERT INTO tibianusapp_monster (monster_name, monster_exp) VALUES (%s, %s)"""
+                    
+                    if('exp' in current_monster):                
+                        if '?' in current_monster['exp'] or '--' in current_monster['exp']:
+                            omitted += 1
+                            continue
                         else:
-                            records = (current_monster['name'], current_monster['exp'].replace(',', ''))
-                        cursor.execute(query, records)
-                        connection.commit()
-                        count += 1
+                            if '~' in current_monster['exp']:
+                                records = (current_monster['name'], current_monster['exp'].replace('~', ''))
+                            else:
+                                records = (current_monster['name'], current_monster['exp'].replace(',', ''))
+                            cursor.execute(query, records)
+                            connection.commit()
+                            count += 1
+                    else:
+                        continue
                 else:
-                    continue
-            else:
-                continue        
+                    continue 
+
+            if connection:
+                cursor.close()
+                connection.close()
+                print(f"Gotowe. Dodano {count_rows} rekordów do bazy danych!\nPominiętych zostało {omitted} rekordów (według wytycznych działania algorytmu).")
+                print('Połączenie z PostgreSQL zostało zamknięte')   
+
     except (Exception, psycopg2.Error) as error:
         print("Błąd w przetwarzaniu żądania: ", error)
     
-    finally:
-        if connection:
-            cursor.close()
-            connection.close()
-            print(f"Gotowe. Dodano {count} potworów do bazy danych!")
-            print('Połączenie z PostgreSQL zostało zamknięte')
+    
+    
 
 
 make_query('https://tibiawiki.dev/api/creatures')
